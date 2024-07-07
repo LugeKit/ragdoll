94c9edd1ca8a535f81d7f9d7a43815baf5f8d54e
LugeK1t <263635591@qq.com>
2024-07-07 11:29:44 +0800

    feat: restore old window

--- component/screen_window.py
+++ component/screen_window.py
@@ -1,10 +1,15 @@
+import win32con
+import win32gui
 from PySide6 import QtWidgets, QtGui
 from PySide6.QtCore import Qt
 
 
 class ScreenWindow(QtWidgets.QMainWindow):
+    _EMPTY_HWND = -1
+
     def __init__(self, parent=None):
         super().__init__(parent)
+        self.hwnd = self._EMPTY_HWND
         self.setWindowFlags(
             Qt.WindowType.FramelessWindowHint
             | Qt.WindowType.CustomizeWindowHint
@@ -20,10 +25,21 @@ class ScreenWindow(QtWidgets.QMainWindow):
 
         self.setLayout(layout)
 
+    def close(self):
+        self._restore_foreground_window()
+        super().close()
+
     def mouseDoubleClickEvent(self, event):
         self.close()
 
     def display(self, img: QtGui.QPixmap | QtGui.QImage):
+        self.hwnd = win32gui.GetForegroundWindow()
         self.label.setPixmap(img)
         self.resize(img.size())
         self.show()
+
+    def _restore_foreground_window(self):
+        if self.hwnd != self._EMPTY_HWND:
+            code = win32gui.ShowWindow(self.hwnd, win32con.SW_RESTORE)
+            print(f"show window {self.hwnd}, code is {code}")
+            self.hwnd = self._EMPTY_HWND
